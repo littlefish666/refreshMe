ddd9b179e381b44c4890d546eb4d98713f7155e7
x <f6runner@gmail.com>
2017-07-17 15:06:33 +0100

    Added source

---
+++ refreshMe/mainWindow.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace refreshMe
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWindow));
+            this.readerPanel = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFile = new System.Windows.Forms.Button();
+            this.readerPnl = new System.Windows.Forms.Label();
+            this.readerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // readerPanel
+            // 
+            this.readerPanel.BackColor = System.Drawing.Color.Transparent;
+            this.readerPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.readerPanel.Controls.Add(this.readerPnl);
+            this.readerPanel.Location = new System.Drawing.Point(0, 51);
+            this.readerPanel.Name = "readerPanel";
+            this.readerPanel.Size = new System.Drawing.Size(1018, 10000);
+            this.readerPanel.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFile
+            // 
+            this.openFile.Location = new System.Drawing.Point(13, 13);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(75, 23);
+            this.openFile.TabIndex = 1;
+            this.openFile.Text = "Open File";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // readerPnl
+            // 
+            this.readerPnl.AutoSize = true;
+            this.readerPnl.Location = new System.Drawing.Point(10, 9);
+            this.readerPnl.Name = "readerPnl";
+            this.readerPnl.Size = new System.Drawing.Size(0, 13);
+            this.readerPnl.TabIndex = 0;
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1041, 599);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.readerPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mainWindow";
+            this.Text = "refreshMe";
+            this.readerPanel.ResumeLayout(false);
+            this.readerPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel readerPanel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.Label readerPnl;
+    }
+}
+
---
+++ refreshMe/mainWindow.cs
@@ -0,0 +1,46 @@
+﻿using MyProg;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace refreshMe
+{
+    public partial class mainWindow : Form
+    {
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            var config = new IniFile("config.ini");
+            config.Write("interval", "50");
+            OpenFileDialog selectFile = new OpenFileDialog();
+            selectFile.Title = "Open file containing only text";
+            selectFile.Filter = "Text file (*.txt)|*.txt|INI (*.ini)|*.ini"
+                    + "|All Files (*.*)|*.*";
+            if (selectFile.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFile = selectFile.FileName; ;
+                config.Write("fileChosen", selectedFile);
+            }       
+            readerPnl.Text = File.ReadAllText(config.Read("fileChosen"));
+            int interval = Convert.ToInt32(config.Read("interval"));
+            var refreshTimer = new Timer { Interval = interval };
+            refreshTimer.Tick += (o, args) =>
+            {
+                readerPnl.Text = File.ReadAllText(config.Read("fileChosen"));
+            };
+            refreshTimer.Start();
+            //Thanks to icons8.com for the icon
+        }
+    }
+}
